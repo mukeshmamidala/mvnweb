590f583532c242c31b6edf10c7e29f5d4ea55a1a
mukeshmamidala <mukeshmamidala@gmail.com>
2019-03-27 00:17:08 -0700

    add

---
+++ src/main/java/com/dbs/training/demoproj/App.java
@@ -0,0 +1,23 @@
+package com.dbs.training.demoproj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public String greet()
+    {
+        
+        int x = 10;
+        
+        
+    	return "Hello";
+    }
+    
+}
